ce2a3df112e6c14223dfa63c9baf01b9d8bbeb7b
Vitrai Gábor <vitraig@gmail.com>
2020-03-25 19:07:54 +0100

    Hero Service Added

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,23 +8,25 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-    level: 1
-  };
 
-  heroes = HEROES;
-
+  heroes = [];
   selectedHero: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { 
+    
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
   
 
